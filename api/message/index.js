a19c173d2527aecd6cee4d02f8b225654223498c
Gabriel Sutherland <gabriel.2sutherland@gmail.com>
2022-06-09 08:34:23 -0700

    commit 6/9/8:34

--- api/message/index.js
+++ api/message/index.js
@@ -1,7 +1,5 @@
 module.exports = async function (context, req) {
-    module.exports = async function (context, req) {
-        context.res.json({
-            text: "Hello from the API"
-        });
-    };
-}
+    context.res.json({
+        text: "Hello from the API"
+    });
+};
